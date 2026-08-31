28c19e8b5e96a39b3e6649469b7c51bbc9883283
maru <35361158+marukitano@users.noreply.github.com>
2026-08-31 22:17:17 +0200

    Add compact Past Present Future number renderer

--- pebble/src/c/ppf_digit_font.h
+++ pebble/src/c/ppf_digit_font.h
@@ -3,6 +3,9 @@
 #include <pebble.h>
 
 #define PPF_VALUE_HEIGHT 21
+#define PPF_SMALL_HEIGHT 11
 
 int ppf_value_width(const char *text);
 void ppf_draw_value(GContext *ctx, const char *text, int right_x, int y, GColor color);
+int ppf_small_value_width(const char *text);
+void ppf_draw_small_value_centered(GContext *ctx, const char *text, GRect box, GColor color);
